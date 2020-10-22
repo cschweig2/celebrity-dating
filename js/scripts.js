5dfcc007ef4168c24a08ea04257004553d9921b7
Chelsea Becker <cschweig2@gmail.com>
2020-10-22 10:55:56 -0700

    Fix found bugs in JS and HMTL.
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,13 @@ $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     const name = $("input#name").val();
     const age = parseInt($("input#age").val());
-    const hair = $("input#hair").val();
-    const gender = $("input#gender").val();
+    const hair = $("select#hair").val();
+    const gender = $("select#gender").val();
 
-    $("#dennis").hide();
-    $("#scarlett").hide();
-
-    if (hair === 'brown' && age > 45 && gender === 'male' || gender === 'either') {
+    if (hair === 'Brown' && age >= 45 && gender === 'Male' || gender === 'Either') {
       $("#dennis").show()
     } 
-    else (hair === 'brown' && age < 45 && gender === 'female' || gender === 'either') {
+    else if (hair === 'Brown' && age <= 45 && gender === 'Female' || gender === 'Either') {
       $("#scarlett").show();
     }
   
