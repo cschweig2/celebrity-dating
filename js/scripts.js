cbde7372f162cdcf43e374756324cd0c4c5a24dc
Chelsea Becker <cschweig2@gmail.com>
2020-10-22 09:45:48 -0700

    Add basic html and starter script
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("form#personalInfo").submit(function(event));
+    const firstName = $("input#firstName").val();
+    const age = parseInt($("input#age").val());
+    const hairColor = $("input#hairColor").val();
+
+
+  });
+});
